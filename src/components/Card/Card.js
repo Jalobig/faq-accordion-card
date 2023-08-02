a0d73b96e1a67a264caa33d1b7d42a0ec3b6cdbe
Jalobig <alexis.jason7@gmail.com>
2023-08-02 16:24:26 -0400

    project completed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,32 +1,40 @@
 import React from "react";
 import classes from "./Card.module.scss";
 import IllustrationWomanDesktop from "../../images/illustration-woman-online-desktop.svg";
+import IllustrationWomanMobile from "../../images/illustration-woman-online-mobile.svg";
 import IllustrationBox from "../../images/illustration-box-desktop.svg";
 import BGDesktop from "../../images/bg-pattern-desktop.svg";
+import BGMobile from "../../images/bg-pattern-mobile.svg";
 import FAQ from "../FAQ/FAQ";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 const DUMMY_FAQ = [
   {
+    id: "q1",
     question: "How many team members can I invite?",
     answer:
       "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
   },
   {
+    id: "q2",
     question: "What is the maximum file upload size?",
     answer:
       "No more than 2GB. All files in your account must fit your allotted storage space.",
   },
   {
+    id: "q3",
     question: "How do I reset my password?",
     answer:
       "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
   },
   {
+    id: "q4",
     question: "Can I cancel my subscription?",
     answer:
       "Yes! Send us a message and we’ll process your request no questions asked.",
   },
   {
+    id: "q5",
     question: "Do you provide additional support?",
     answer:
       "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
@@ -34,27 +42,39 @@ const DUMMY_FAQ = [
 ];
 
 const Card = () => {
+    const media = useMediaQuery('only screen and (max-width: 1200px)')
   return (
     <div className={classes.card}>
       <div className={classes.card__composition}>
-        <img
-          src={BGDesktop}
+        <div className={classes['card__composition--insidecard']}>
+        {media?  <><img
+          src={BGMobile}
           alt="Background design"
           className={classes["card__composition--1"]}
         />
         <img
-          src={IllustrationWomanDesktop}
+          src={IllustrationWomanMobile}
           alt="Illustration of a woman in front of a huge screen"
           className={classes["card__composition--2"]}
-        />
-        <img
+        /></> :<><img
+        src={BGDesktop}
+        alt="Background design"
+        className={classes["card__composition--1"]}
+      />
+      <img
+        src={IllustrationWomanDesktop}
+        alt="Illustration of a woman in front of a huge screen"
+        className={classes["card__composition--2"]}
+      /></>}
+        </div>
+        {media || <img
           src={IllustrationBox}
           alt="Illustration of a box"
           className={classes["card__composition--3"]}
-        />
+        />}
       </div>
       <div>
-        <FAQ  questionAnswers={DUMMY_FAQ}/>
+        <FAQ questionAnswers={DUMMY_FAQ} />
       </div>
     </div>
   );
--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,24 +1,110 @@
-import React from 'react'
-import classes from './FAQ.module.scss'
-import IconArrowDown from '../../images/icon-arrow-down.svg'
+import React, { useState } from "react";
+import classes from "./FAQ.module.scss";
+import IconArrowDown from "../../images/icon-arrow-down.svg";
 
 const FAQ = (props) => {
+  const [questionChoice, setQuestionChoice] = useState({});
+  const accordionHandler = (id) => {
+    setQuestionChoice({
+      q1: false,
+      q2: false,
+      q3: false,
+      q4: false,
+      q5: false,
+    });
+
+    if (id === "q1") {
+      setQuestionChoice({
+        q1: true,
+        q2: false,
+        q3: false,
+        q4: false,
+        q5: false,
+      });
+    } else if (id === "q2") {
+      setQuestionChoice({
+        q1: false,
+        q2: true,
+        q3: false,
+        q4: false,
+        q5: false,
+      });
+    } else if (id === "q3") {
+      setQuestionChoice({
+        q1: false,
+        q2: false,
+        q3: true,
+        q4: false,
+        q5: false,
+      });
+    } else if (id === "q4") {
+      setQuestionChoice({
+        q1: false,
+        q2: false,
+        q3: false,
+        q4: true,
+        q5: false,
+      });
+    } else if (id === "q5") {
+      setQuestionChoice({
+        q1: false,
+        q2: false,
+        q3: false,
+        q4: false,
+        q5: true,
+      });
+    }
+  };
   return (
     <div className={classes.faq}>
-        <h1 className={classes.faq__heading}>FAQ</h1>
-        <div className={classes.faq__accordion}>
-            {props.questionAnswers.map((qa)=> {
-                return <>
-                <div className={classes.faq__qrow}>
-                <p className={`${classes.faq__question}`}>{qa.question}</p>
-                <img src={IconArrowDown} alt='Icon caret' className={`${classes.faq__icon}`}/>
-                </div>
-                <hr/>
-                </>
-            })}
-        </div>
+      <h1 className={classes.faq__heading}>FAQ</h1>
+      <div className={classes.faq__accordion}>
+        {props.questionAnswers.map((qa) => {
+          return (
+            <div key={qa.id}>
+              <div
+                className={`${classes.faq__qrow} ${
+                  questionChoice[qa.id] && classes.active
+                }`}
+                onClick={() => {
+                  if (!questionChoice[qa.id]) {
+                    accordionHandler(qa.id);
+                  } else {
+                    setQuestionChoice({
+                      q1: false,
+                      q2: false,
+                      q3: false,
+                      q4: false,
+                      q5: false,
+                    });
+                  }
+                }}
+              >
+                <p
+                  className={`${classes.faq__question} ${
+                    questionChoice[qa.id] && classes.active
+                  }`}
+                >
+                  {qa.question}
+                </p>
+                <img
+                  src={IconArrowDown}
+                  alt="Icon caret"
+                  className={`${classes.faq__icon} ${
+                    questionChoice[qa.id] && classes.active__icon
+                  }`}
+                />
+              </div>
+              {questionChoice[qa.id] && (
+                <p className={`${classes.faq__answer}`}>{qa.answer}</p>
+              )}
+              <hr />
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
